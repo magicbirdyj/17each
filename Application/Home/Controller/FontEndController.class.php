e5e27f7e25d5ca7051eeeeb251be6847a3631080
yangjing <12493085@qq.com>
2016-05-03 12:29:22 +0800

    手机测试

--- Application/Home/Controller/FontEndController.class.php
+++ Application/Home/Controller/FontEndController.class.php
@@ -76,7 +76,7 @@ HTML;
         if($_SERVER['HTTP_HOST']==='m.17each.com'){
             C("DEFAULT_THEME", "Mobile");//默认模板主题名称
             C("TMPL_CACHE_PREFIX", "mb");//模板缓存前缀标志
-        }else if ($ismobile) {
+        }else{
             //$this->redirect("m.17each.com"); 
             header("location: http://m.17each.com");
         }
