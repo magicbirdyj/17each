57c40952fc97e16b1c024ffebf8c98295df53a1f
yangjing <12493085@qq.com>
2016-07-03 10:07:10 +0800

    测试

--- Application/Home/Controller/FontEndController.class.php
+++ Application/Home/Controller/FontEndController.class.php
@@ -146,6 +146,7 @@ HTML;
         $token_access_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" .APPID. "&secret=" .APPSECRET;
         $res = file_get_contents($token_access_url); //获取文件内容或获取网络请求的内容
         $result = json_decode($res, true); //接受一个 JSON 格式的字符串并且把它转换为 PHP 变量
+        var_dump($result);
         $access_token = $result['access_token'];
         S('access_token',$access_token,7000);
     }
