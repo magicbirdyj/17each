77e29e1336f39a62ca167aca3fdec1c9919f9d25
yangjing <12493085@qq.com>
2016-07-05 22:49:18 +0800

    完成题库

--- Application/Home/Controller/Yj921314Controller.class.php
+++ Application/Home/Controller/Yj921314Controller.class.php
@@ -25,50 +25,7 @@ class Yj921314Controller extends FontEndController {
         
         $this->display();
     }
-    public function danxuan(){
-        //$arr=array(
-            //0=>'√',
-            //1=>'×'
-        //);
-        //var_dump(serialize($arr));
-        //exit();361  390
-        
-        
-        
-        
-        $str=file_get_contents('ceshi.txt');
-        //$str=str_replace(array("\r\n", "\r", "\n"), "timufenge", $str);$arr=  preg_split('/\n|\r\n/',$str);
-        $arr=  split('mmmmmm',$str);
-        $tikumodel=D('Tiku');
-        foreach ($arr as $value) {
-            $arr_1=split('参考答案：',$value);
-            $daan=$arr_1[1];
-            $arr_2=  preg_split('/\n|\r\n/',$arr_1[0]);
-            array_pop($arr_2) ;
-            $timu=substr($arr_2[0],strpos($arr_2[0],'、')+3);
-            //$timu=substr($arr_2[0],strpos($arr_2[0],'.')+1);
-            array_shift($arr_2);
-            foreach ($arr_2 as &$value2){
-                $value2=str_replace(" ","",$value2);
-            }
-            $xuanxiang=serialize($arr_2);
-            $row=array(
-                'zhangjie'=>'关于党和国家工作人员操办婚丧喜庆事宜的暂行规定',
-                'tixing'=>5,
-                'timu'=>$timu,
-                'xuanxiang'=>'',
-                'daan'=>$daan
-            );
-            var_dump($row);
-            
-            //$result=$tikumodel->add($row);
-            if($result){
-                echo '输入数据库成功</br>';
-            }
-
-        }
-        
-    }
+    
 
 
 }
