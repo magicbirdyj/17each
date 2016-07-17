d23a0de312111105bce1279d6add8d498705f9cb
yangjing <12493085@qq.com>
2016-07-17 12:51:28 +0800

    测试

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -115,7 +115,6 @@ class LoginController extends FontEndController {
             //var_dump($userinfo);
             $usersmodel=D('Users');
             $count=$usersmodel->where("open_id='$open_id'")->count();
-            var_dump($count);exit();
             if($count==='0'){
                 $row=array(
                     'open_id'=>$open_id,
@@ -128,7 +127,7 @@ class LoginController extends FontEndController {
                 $user=$usersmodel->where("open_id='$open_id'")->field('user_id,user_name,shopman_id')->find();
                 var_dump($user);exit();
                 $_SESSION['huiyuan']=array(
-                    'user_id'=>$user['id'],
+                    'user_id'=>$user['user_id'],
                     'user_name'=>$user['user_name'],
                     'open_id'=>$open_id,
                     'shopman_id'=>$user['shopman_id']
