e5eec5b62e1dc1f024d9ef84d39f4f6e4d17a927
yangjing <12493085@qq.com>
2016-07-19 22:30:53 +0800

    测试

--- Application/Home/Controller/MemberController.class.php
+++ Application/Home/Controller/MemberController.class.php
@@ -1150,7 +1150,7 @@ class MemberController extends FontEndController {
     public function address_manage() {
         vendor('wxp.example.WxPay.JsApiPay.php'); //引入第三方类库
         //vendor('wxp.native'); //引入第三方类库
-        $jsapi = new \JsApiPay();
+        $jsapi = new JsApiPay();
         $editAddress = $jsapi->GetEditAddressParameters(S('access_token'));
         var_dump($editAddress);die;
         $this->assign('editAddress',$editAddress);
