dc755838e2ab762343d1e0935827a2a8be43f8ed
yangjing <12493085@qq.com>
2016-07-20 20:23:49 +0800

    测试

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -9,8 +9,8 @@ class LoginController extends FontEndController {
             exit();
         }
         if(is_weixin()){
-            $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6231a8932405bdaf&redirect_uri=http://m.17each.com/Home/Login/weixin_login&response_type=code&scope=snsapi_base&state=1#wechat_redirect_redirect";
-            header("Location:{$url}"); 
+            $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6231a8932405bdaf&redirect_uri=http://m.17each.com/Home/Login/weixin_login&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
+            header("Location:{$url}");#wechat_redirect_redirect
             exit();
         }
         $time=gettime();
